5dbaf520d6fc263e69640310f9c41f5d4b05a156
fontes7 <hfontes777@gmail.com>
2024-02-28 15:25:44 +0000

    getExerciseById function tested

--- code/backend/data/local/data.ts
+++ code/backend/data/local/data.ts
@@ -2,14 +2,17 @@ import fs from "node:fs/promises";
 import { Exercise } from "../../domain/types";
 
 async function getLocalData(path: string) {
-    const data = await fs.readFile(path);
-    return JSON.parse(data.toString());
+  const data = await fs.readFile(path);
+  return JSON.parse(data.toString());
 }
 
 export class Data {
   async getExerciseById(id: string) {
-    const exercises = await getLocalData("data/local/exercises.json") as ExerciseDB[];
-    const exercise = da
+    const exercises = (await getLocalData(
+      "data/local/exercises.json"
+    )) as Exercise[];
     
+    const exercise = exercises.find((exercise) => exercise._id === id);
+    return exercise || null
   }
 }
---
+++ code/backend/tests/services.test.ts
@@ -0,0 +1,25 @@
+import { Api } from "../api/api.ts";
+import { Services } from "../services/services.ts";
+import { Data } from "../data/local/data.ts";
+import { NotFoundError } from "../errors/app_errors.ts";
+import { NOTFOUND } from "node:dns";
+
+const data = new Data();
+const service = new Services(data);
+const api = new Api(service);
+
+describe("getExerciseById function tests", () => {
+  test("getExerciseById returns the exercise successfully", () => {
+    const id = "0001";
+    service
+      .getExerciseById(id)
+      .then((exercise) => expect(exercise._id).toBe(id));
+  });
+
+  test("getExerciseById throws not found when exercise does not exist", () => {
+    service
+      .getExerciseById("1000000000")
+      .catch((error) => expect(error).toBe(NotFoundError));
+  });
+});
+
