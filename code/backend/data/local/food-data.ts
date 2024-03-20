b43c90d5e9eee9413bb1aacbb387bb48f3136bb9
fontes7 <hfontes777@gmail.com>
2024-03-20 17:53:48 +0000

    searchFood services function tested

--- code/backend/data/local/food-data.ts
+++ code/backend/data/local/food-data.ts
@@ -2,14 +2,9 @@ import { IFoodData } from "../../domain/interfaces";
 import { Food, FoodFactsApiFood } from "../../domain/types";
 import { getLocalData } from "../../utils/functions/data";
 
-
 export class LocalFoodData implements IFoodData {
-    async searchFood(query: string, skip: number, limit: number) {
-        
-
-
-        return (await getLocalData(
-            "data/local/files/exercises.json"
-          )) as FoodFactsApiFood[];
-    }
+  async searchFood(query: string, skip: number, limit: number) {
+    const { products } = await getLocalData(`data/local/files/food/${query}.json`);
+    return products as FoodFactsApiFood[]
   }
+}
