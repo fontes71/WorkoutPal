9fcf2c9d3744519eebd72e1f9dcf85408b44bc37
Aureliu555 <A48285@alunos.isel.pt>
2024-04-28 15:51:35 +0100

    first screen when entering the app now is the exercise screen

--- code/frontend/app/index.tsx
+++ code/frontend/app/index.tsx
@@ -1,30 +1,7 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import ExerciseScreen from './(tabs)/exercise';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle="dark-content" />
-      <Link href="/(tabs)/exercise">
-          <Text style={styles.linkText}>Get Started</Text>
-      </Link>
-      <Link href="/(auth)/login">
-          <Text style={styles.linkText}>Log In</Text>
-      </Link>
-    </View>
+    <ExerciseScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#1F2123',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  linkText: {
-    fontSize: 35,
-    color: '#2e78b7',
-  },
-});
---
+++ code/frontend/app/utils/components/auth/PasswordInput.tsx
@@ -0,0 +1,51 @@
+import React, { useState, Dispatch, SetStateAction } from 'react';
+import { View, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+
+interface PasswordInputProps {
+    readonly password: string;
+    readonly setPassword: Dispatch<SetStateAction<string>>;
+}
+
+export default function PasswordInput({password, setPassword}: PasswordInputProps) {
+    const [showPassword, setShowPassword] = useState(false)
+
+    return (
+        <View style={styles.input_container}>
+            <TextInput
+                style={styles.input}
+                onChangeText={setPassword}
+                value={password}
+                placeholder="Password"
+                secureTextEntry={!showPassword}
+                autoCapitalize="none"
+            /> 
+            <TouchableOpacity style={ styles.show_password_button } onPress={() => setShowPassword(prevShowPassword => !prevShowPassword)}>
+                    {showPassword ? <FontAwesome style={styles.show_password_icon} name="eye" size={15}/> : <FontAwesome style={styles.show_password_icon} name="eye-slash" size={15}/>}     
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    input_container: {
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        borderRadius: 10,
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: 'gray',
+        paddingHorizontal: 20,
+    },
+    input: {
+        width: '93%',
+        height: 41,
+    },
+    show_password_button: {
+        height: '100%',
+        flexDirection: 'row'
+    },
+    show_password_icon: {
+        alignSelf: 'center'
+    },
+})
