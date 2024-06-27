023131b4e9ee6857840837a0ca81c1529d216771
fontes7 <hfontes777@gmail.com>
2024-06-27 12:59:23 +0100

    quantityToPresent food property removed and tests fixed for that

--- code/frontend/domain/food.ts
+++ code/frontend/domain/food.ts
@@ -2,14 +2,13 @@ type Food = {
     id: string;
     name: string;
     brand: string;
-    quantityToPresent: string;
     quantity: string,
     quantityUnit: string;
     imageUrl: string;
     calories: number | null;
     protein: string | null;
     fat: string | null;
-    carbs: string | null;
+    carbs: string | null; 
     fiber: string | null;
     saturatedFat: string | null;
     salt: string | null;
