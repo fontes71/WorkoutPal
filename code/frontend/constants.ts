ca80620c968c5d7857514d9fa3c186fe4e5032b2
Aureliu555 <A48285@alunos.isel.pt>
2024-06-27 21:21:41 +0100

    chart template done

--- code/frontend/constants.ts
+++ code/frontend/constants.ts
@@ -1,27 +1,12 @@
 export const localhost = process.env.EXPO_PUBLIC_LOCALHOST
 
-const tintColorLight = "#2f95dc";
-const tintColorDark = "#fff";
-
 export const Colors = {
-  light: {
-    text: "#000",
-    background: "#fff",
-    tint: tintColorLight,
-    tabIconDefault: "#ccc",
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: "#fff",
-    background: "#000",
-    tint: tintColorDark,
-    tabIconDefault: "#ccc",
-    tabIconSelected: tintColorDark,
-  },
   blue: "#1a74e2",
   midBlue: "#3fa0ef",
   lightBlue: "#6bd0ff",
   lightBlack: "#1f2123",
   black: "#101010",
-  gray: "#D9D9D9"
+  gray: "#D9D9D9",
+  green: "#00ff00",
+  white: "#ffffff"
 };
