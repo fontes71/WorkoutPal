2e586be2ef7ea755420e04d31fb49850b89c9b59
fontes7 <hfontes777@gmail.com>
2024-07-02 15:39:21 +0100

    isValidDate changed

--- code/backend/utils/functions/app/isValidDate.ts
+++ code/backend/utils/functions/app/isValidDate.ts
@@ -1,25 +1,9 @@
-export const isValidDate = (dateString: string) => {
-    // Regular expression to match the format `dd-mm-yyyy`
-    const regex = /^(\d{1,2})-(\d{1,2})-(\d{4})$/;
+import { isValid, parse } from "date-fns";
 
-    // Test the string against the regex
-    const match = dateString.match(regex);
+const isValidDate = (date: string) => {
+    const parsedDate = parse(date, "yyyy-MM-dd", new Date())
+    return isValid(parsedDate)
 
-    if (!match) {
-        return false;
-    }
-
-    // Extract day, month, year from the regex match groups
-    const day = parseInt(match[1], 10);
-    const month = parseInt(match[2], 10);
-    const year = parseInt(match[3], 10);
-
-    // Check if the day, month, year are in valid ranges
-    if (day < 1 || day > 31 || month < 1 || month > 12 || year < 1000 || year > 9999) {
-        return false;
-    }
-
-    // Additional check for valid days in month (considering leap years)
-    const date = new Date(year, month - 1, day);
-    return date.getFullYear() === year && date.getMonth() === month - 1 && date.getDate() === day;
 }
+
+export default isValidDate
