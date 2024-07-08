1414fb47124e629c294d7f12130b52534bf9825d
Aureliu555 <A48285@alunos.isel.pt>
2024-07-08 14:05:38 +0100

    layout optimized

--- code/frontend/assets/components/layouts/MainTabsLayout.tsx
+++ code/frontend/assets/components/layouts/MainTabsLayout.tsx
@@ -4,6 +4,7 @@ import { Slot } from "expo-router";
 import Footer from "./MainFooter";
 import Header from "./MainHeader";
 import { Colors } from "@/assets/constants";
+import layout_styles from "@/assets/styles/layout";
 
 type TabsLayoutOptions = {
   readonly screenName: string
@@ -21,11 +22,15 @@ function MainTabsLayout({screenName}: TabsLayoutOptions) {
 };
 
 export default function MainTabsLayoutDarkModeTest({screenName}: TabsLayoutOptions) {
+  const styles = layout_styles.generic_styles
+
   return (
     <View style={{flex: 1, backgroundColor: Colors.black}}>
       <StatusBar backgroundColor={Colors.lightBlack}/>
       <Header isMainScreen={true} screenName={screenName}></Header>
-      <Slot></Slot>
+      <View style={styles.children_slot}>
+        <Slot></Slot>
+      </View>
       <Footer></Footer>
     </View>
   );
