1f8c41ef5d7e2570cfd47d9692896c53e5d89f50
Miguel Agostinho <A48338@alunos.isel.pt>
2024-07-11 15:55:14 +0100

    Fixed exercise details screen

--- code/frontend/app/fitness/exercise-details/[id].tsx
+++ code/frontend/app/fitness/exercise-details/[id].tsx
@@ -16,49 +16,50 @@ const ExerciseDetailsScreen = () => {
 
   const [modalVisible, setModalVisible] = useState(false);
 
+
   return (
-    <View>
-      <Stack.Screen options={{ title: "Details" }} />
-      <View style={exercise_screen_styles.exercisesResultContainer}>
-        <View style={exercise_screen_styles.exerciseResultTextContainer}>
-          <Text style={exercise_screen_styles.topText}>{exercise.name}</Text>
-          <View style={exercise_screen_styles.imageContainer}>
-            {exercise.gifUrl && (
-              <Image
-                style={exercise_screen_styles.exerciseGifResult}
-                source={{ uri: exercise.gifUrl }}
-              />
-            )}
-          </View>
-          <BottomText str={"Equipment: " + exercise.equipment} />
-          <BottomText str={"Body Part: " + exercise.bodyPart} />
-          <BottomText str={"Target: " + exercise.target} />
-          <BottomText
-            str={"Secondary Muscles: " + exercise.secondaryMuscles.join(", ")}
-          />
-          <Text style={exercise_screen_styles.topText}></Text>
-          <Text style={exercise_screen_styles.topText}>Instructions:</Text>
-          {exercise.instructions.map((instruction, index) => (
-            <Text key={index} style={exercise_screen_styles.bottomText}>
-              {instruction}
-            </Text>
-          ))}
-          <Button
-            onPress={() => {
-              setModalVisible(true);
-            }}
-            color={Colors.blue}
-          >
-            Add To Workout Plan
-          </Button>
-          <WorkoutPlansModalScreen
-            isVisible={modalVisible}
-            onClose={() => {
-              setModalVisible(false);
-            }}
-            exerciseId={exercise._id}
-          />
+    <View style={{ flex: 1 }}>
+      <View style={{ padding: 20, alignItems: "center" }}>
+        <Text style={exercise_screen_styles.title}>{exercise.name}</Text>
+        <Text style={exercise_screen_styles.title}></Text>
+        <View style={exercise_screen_styles.imageContainer}>
+          {exercise.gifUrl && (
+            <Image
+              style={exercise_screen_styles.exerciseGifResult}
+              source={{ uri: exercise.gifUrl }}
+            />
+          )}
         </View>
+        <Text style={exercise_screen_styles.topText}></Text>
+        <BottomText str={"Equipment: " + exercise.equipment} />
+        <BottomText str={"Body Part: " + exercise.bodyPart} />
+        <BottomText str={"Target: " + exercise.target} />
+        <BottomText
+          str={"Secondary Muscles: " + exercise.secondaryMuscles.join(", ")}
+        />
+        <Text style={exercise_screen_styles.topText}></Text>
+        <Text style={exercise_screen_styles.topText}>Instructions:</Text>
+        {exercise.instructions.map((instruction, index) => (
+          <Text key={index} style={exercise_screen_styles.bottomText}>
+            {instruction}
+          </Text>
+        ))}
+        <Text style={exercise_screen_styles.topText}></Text>
+        <Button
+          onPress={() => {
+            setModalVisible(true);
+          }}
+          color={Colors.blue}
+        >
+          Add To Workout Plan
+        </Button>
+        <WorkoutPlansModalScreen
+          isVisible={modalVisible}
+          onClose={() => {
+            setModalVisible(false);
+          }}
+          exerciseId={exercise._id}
+        />
       </View>
     </View>
   );
